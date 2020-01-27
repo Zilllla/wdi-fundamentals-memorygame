36dd7a234596f5f0ec7baa99604f696cc67d8edf
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-01-27 04:33:06 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,28 @@
-console.log("Up and running!");
+/*console.log("Up and running!");
 
 let cardOne = "queen";
 let cardTwo = "queen";
 let cardThree = "king";
-let cardFour = "king";
+let cardFour = "king"; 
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+console.log("User flipped " + cardThree); */
+
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+let cardOne = cards[3];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);	
+
+if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
