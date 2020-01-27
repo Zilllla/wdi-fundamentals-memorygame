e1b1ea9779aaae7db7de70f564cd8ba3e551ee6a
Courtney Zavaleta <courtneyzavaleta@gmail.com>
2020-01-27 11:50:18 +0000

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,6 @@
-/*console.log("Up and running!");
-
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king"; 
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree); */
 
 
-const cards = [
+let cards = [
 {
 	rank: "queen",
 	suit: "hearts",
@@ -31,54 +22,42 @@ const cards = [
 	cardImage: "images/king-of-diamonds.png"
 },
 ];
-/*
-function cardImage() {
-	console.log(cardImage);
-}
-	
-function suit() {
-	console.log(suit);
-}
-*/	
 
-const cardsInPlay = [];
+
+let cardsInPlay = [];
 
 
 
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-  console.log("You found a match!");
+  alert("You found a match!");
 } else {
-  console.log("Sorry, try again.");
+  alert("Sorry, try again.");
 }
 };
 
-checkForMatch(); 
 
-function flipCard(cardId) {
+function flipCard() {
+	let cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
 	console.log("User flipped " + cards[cardId].rank);
-	console.log(cards[cardID].cardImage);
-	console.log(cards[cardID].suit);
-	
-	
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);	
+	this.setAttribute('src', cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();  
+	}
 };
 	
-
-
-flipCard(0);
-
-flipCard(2);
-
-
-
-/*
-
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		let cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
 }
-else {
-	alert("Sorry, try again.");
-}
-*/
+
+createBoard();
+
